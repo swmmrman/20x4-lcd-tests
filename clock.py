0cca8434b457fd66366704e025f10e5f1ec858e0
Stephan Kern <stephan@swmmrman.com>
2021-08-16 16:12:31 -0600

    Update Current to just say time, and chage variable names

--- clock.py
+++ clock.py
@@ -4,13 +4,13 @@ import time
 lcd = LCD()
 lines = []
 while True:
-    c_time = time.time()
-    c_time_str = time.strftime("%H:%M:%S", time.localtime(c_time))
+    t = time.time()
+    t_str = time.strftime("%H:%M:%S", time.localtime(t))
     d_time = time.monotonic()
     d_time_str = time.strftime(":%H:%M:%S", time.gmtime(d_time))
     d_days = int(time.strftime("%j", time.gmtime(d_time)))-1
     date = time.strftime('%Y-%m-%d')
     lcd.text(f"Date      {date} ",1)
-    lcd.text(f"Current     {c_time_str}", 2)
+    lcd.text(f"Time        {t_str}", 2)
     lcd.text(f"Uptime  {d_days:3d}{d_time_str}", 4)
     time.sleep(.4)
